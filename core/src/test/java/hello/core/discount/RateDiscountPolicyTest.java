2f9a14477234ced500a417c42632817345a7dba2
박호영 <you@example.com>
2025-04-21 01:22:21 +0900

    테스트 학습

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -16,19 +16,16 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP는 10% 할인이 적용되어야 한다")
     void vip_o(){
         Member member = new Member(1L, "memberVIP", Grade.VIP);
-
         int discount = discountPolicy.discount(member, 10000);
-
         assertEquals(1000, discount);
     }
-
     @Test
+
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x(){
         Member member = new Member(1L, "memberBASIC", Grade.BASIC);
-
         int discount = discountPolicy.discount(member, 10000);
-
         assertEquals(0 , discount);
     }
+
 }
